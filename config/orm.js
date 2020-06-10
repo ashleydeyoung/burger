d96fe7649eb09460f6bf5b79d1cc78851a91a70b
Ashley DeYoung <ashley.d.deyoung@gmail.com>
2020-06-09 21:48:02 -0400

    starter for handlebars, orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,12 +1,28 @@
 const connection = require("../config/connection.js");
 
-function selectAll();
+function selectAll() {
+    var query = "SELECT * FROM burgers";
+    connection.query(query, function (err, result) {
+        if (err) throw err;
+        return result
+    })
+};
 
-function insertOne();
+function insertOne(burger) {
+    var query = "INSERT INTO burgers (burger) VALUES (?)";
+    connection.query(query, [burger], function (err, result) {
+        if (err) throw err;
+        return result
+    })
+};
 
-function updateOne();
-
-
+function updateOne(burger, id) {
+    var query = "UPDATE burgers SET burger = ? where id =?";
+    connection.query(query, [burger], id, function (err, result) {
+        if (err) throw err;
+        return result
+    })
+}
 
 
 
