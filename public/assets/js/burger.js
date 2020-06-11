c06d942d42e3a22ee8500350db3b7716858a5e1f
Ashley DeYoung <ashley.d.deyoung@gmail.com>
2020-06-11 12:47:31 -0400

    fixed onclick

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -15,4 +15,22 @@ $(function() {
       );
     });
 
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      var  burger_name =  $("#name")
+  
+      // Send the POST request.
+      $.ajax("/burgers", {
+        type: "POST",
+        data: burger_name
+      }).then(
+        function() {
+          console.log("created new burger!");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
 })
