74c1345616d46f06e3f54d2bde18341794965a63
Ashley DeYoung <ashley.d.deyoung@gmail.com>
2020-06-10 23:08:55 -0400

    JAWS added

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,29 @@
 //setup MySQL connection
-const mysql = require ("mysql");
-var connection = mysql.createConnection({
+const mysql = require("mysql");
+var connection;
+
+//Heroku
+if (process.env.JAWSDB_UR) {
+  connection = mysql.createConnection(process.env.JAWSDB_UR);
+} else {
+  connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "rootroot",
-    database: "burgers_db"
+    database: "burgers_db",
   });
-  
-  //makes connection
-  connection.connect(function(err) {
-    if (err) {
-      console.error("error connecting: " + err.stack);
-      return;
-    }
-  
-    console.log("connected as id " + connection.threadId);
-  });
-  
+}
 
-  //export to orm
-  module.exports = connection;
+//makes connection
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+
+  console.log("connected as id " + connection.threadId);
+});
+
+//export to orm
+module.exports = connection;
